4857b9dd85a15d45e12b46885f3b45b248774812
benikib <benikasu7@gmail.com>
2024-09-17 21:05:36 +0200

    integration de page authentification

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,13 @@
-@extends('layouts.app')
+@extends('layouts_admin.app')
 @section('content')
 
- 
+
     <div class="container-fluid py-4">
         @include('admin.partial.indicator')
         @include('admin.partial.graphique')
-      
-   
+
+
     </div>
-  
-  
+
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('homePage');
-});
-Route::get('/admin', function () {
-    return view('admin.dashboard');
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
-Route::get('/home', function () {
-    return view('homePage');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/indexProf', function () {
-    return view('prof.index');
-});
-
-Route::get('/login', function () {
-    return view('auth.login');
-})->name("login");
-
-Route::get('/create', function () {
-    return view('ecole.create');
-})->name("create");
-
-
-
-
-
-
-
-
+require __DIR__.'/auth.php';
